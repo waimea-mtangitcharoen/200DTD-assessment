7fa9349e7778e27d62f48949d422fead9ada3d5f
mimi tangitcharoen <mtangitcharoen@waimea.school.nz>
2024-08-14 14:06:29 +1200

    styelle
    tgfdht

--- site/idea-list.php
+++ site/idea-list.php
@@ -70,7 +70,7 @@ else {
             echo '<a href="'. $idea['link'] .'">';
             echo '<i data-feather="external-link"></i>';
             echo '</a>';
-            echo   '<a href="form-edit.php?id=' . $idea['id'] . '&area=' . $areaID . '"><i data-feather="edit"></i>';
+            echo   '<a href="form-edit.php?id=' . $idea['id'] . '&area=' . $areaID . '"><i data-feather="edit"></i></a>';
             if($adminPortal == true){
             echo   '<a href="delete-ideas.php?id=' .  $idea['id'] . '&area=' . $areaID . '"><i data-feather="trash-2"></i></a>';
             // echo   '<a href="form-edit.php?id=' . $idea['id'] . '&area=' . $areaID . '"><i data-feather="edit"></i>';
